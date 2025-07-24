9bbd670fb18636780f4323945c2acb3290afba3e
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-07-24 20:46:55 +0530

    annotation

--- src/main/java/com/javaDev/week1/introduction/introToSpringBoot/AppConfig.java
+++ src/main/java/com/javaDev/week1/introduction/introToSpringBoot/AppConfig.java
@@ -2,8 +2,9 @@ package com.javaDev.week1.introduction.introToSpringBoot;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
-@Configuration
+@Service
 public class AppConfig {
 //    constimized bean creation in the config file
 //    @Bean
