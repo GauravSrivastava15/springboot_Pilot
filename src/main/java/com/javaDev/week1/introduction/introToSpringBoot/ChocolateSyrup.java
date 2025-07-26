8cc08f3a76d631e6db1260a30f1f000ec184980f
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2025-07-26 21:02:42 +0530

    ConditionalOnProperty

--- src/main/java/com/javaDev/week1/introduction/introToSpringBoot/ChocolateSyrup.java
+++ src/main/java/com/javaDev/week1/introduction/introToSpringBoot/ChocolateSyrup.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnProperty(name = "flavour.env", havingValue = "chocolate")
+// we can use @ConditionalOnProperty to render conditional beans using env variables
 public class ChocolateSyrup implements Syrup{
     @Override
     public String getSyrupType(){
